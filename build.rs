c36fb7523fcca4b97d50321cc059dc2084bc141a
Cameron Eldridge <cameldridge+git@gmail.com>
2019-02-22 00:38:16 -0500

    add the rest of the languages

--- build.rs
+++ build.rs
@@ -3,6 +3,7 @@ use std::path::Path;
 use std::fs::{self, File};
 use std::io::Write;
 use std::ffi::OsString;
+use std::process::Command;
 use std::env;
 
 use serde::Deserialize;
@@ -15,20 +16,31 @@ struct Package {
 }
 
 fn main() {
+    println!("cargo:rerun-if-changed=package.json");
+    println!("cargo:rerun-if-changed=package-lock.json");
+    println!("cargo:rerun-if-changed=node_modules");
+
     let out_dir = env::var("OUT_DIR").unwrap();
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let languages_path = Path::new(&out_dir).join("languages.rs");
-    let mut output_list = File::create(&languages_path).unwrap();
 
-    let grammar_pattern = Regex::new(r"tree-sitter-(\w+)").unwrap();
-    let data = fs::read_to_string("package.json").expect("The package.json cannot be read");
+    let languages_path = Path::new(&out_dir).join("languages.rs");
+    let node_modules = Path::new(&manifest_dir).join("node_modules");
+
+    let result = Command::new("npm").arg("install").status().expect("Failed to run `npm install`. Do you have npm installed?");
+    if !result.success() {
+        panic!("Failed to run npm install");
+    }
+
+    let grammar_pattern = Regex::new(r"tree-sitter-(.*)").unwrap();
+    let data = fs::read_to_string(Path::new(&manifest_dir).join("package.json")).expect("The package.json cannot be read");
     let package: Package = serde_json::from_str(&data).expect("The package.json is invalid.");
+    let mut output_list = File::create(&languages_path).unwrap();
     for (package_name, _version) in package.dev_dependencies.into_iter() {
         if let Some(captures) = grammar_pattern.captures(&package_name) {
             let language = captures.get(1).unwrap().as_str();
-            writeln!(output_list, "extern \"C\" {{ fn tree_sitter_{}() -> Language; }}", language).unwrap();
+            writeln!(output_list, "extern \"C\" {{ fn tree_sitter_{}() -> Language; }}", language.replace("-", "_")).unwrap();
 
-            let node_module = Path::new(&manifest_dir).join(&format!("node_modules/tree-sitter-{}/src", language));
+            let node_module = Path::new(&node_modules).join(&format!("tree-sitter-{}/src", language));
             for file in fs::read_dir(&node_module).expect("Node modules are not installed") {
                 let mut build = cc::Build::new();
                 match file {
--- src/language.rs
+++ src/language.rs
@@ -5,10 +5,25 @@ include!(concat!(env!("OUT_DIR"), "/languages.rs"));
 pub fn parse<I: AsRef<str>>(name: I) -> Option<Language> {
     let name = name.as_ref();
     unsafe {
+        if name.trim() == "C" {
+            // this one case sensitive one...
+            return Some(tree_sitter_cpp())
+        }
         match name.trim().to_lowercase().as_str() {
             "rs" | "rust" => Some(tree_sitter_rust()),
+            "rb" | "ruby" => Some(tree_sitter_ruby()),
+            "erb" | "ejs" => Some(tree_sitter_embedded_template()),
             "js" | "javascript" => Some(tree_sitter_javascript()),
+            "ts" | "typescript" => Some(tree_sitter_typescript()),
             "sh" | "bash" => Some(tree_sitter_bash()),
+            "py" | "python" | "python3" => Some(tree_sitter_python()),
+            "go" => Some(tree_sitter_go()),
+            "php" => Some(tree_sitter_php()),
+            "ml" | "mli" | "ocaml" => Some(tree_sitter_ocaml()),
+            "c" | "h" => Some(tree_sitter_c()),
+            "cpp" | "cc" | "hpp" | "hh" | "c++" => Some(tree_sitter_cpp()),
+            "html" => Some(tree_sitter_html()),
+            "css" => Some(tree_sitter_css()),
             _ => None
         }
     }
