2d2990aa3305e0f4c94a50ce3714021f22afacf8
Cameron Eldridge <cameldridge+git@gmail.com>
2020-03-14 17:16:38 -0400

    Finish implementing negative nodes

--- syncat-stylesheet/src/stylesheet/query.rs
+++ syncat-stylesheet/src/stylesheet/query.rs
@@ -1,6 +1,6 @@
-use std::ops::{Index, IndexMut};
-use crate::ast::{Node, NodeKind, NodeModifier};
 use super::Matches;
+use crate::ast::{Node, NodeKind, NodeModifier};
+use std::ops::{Index, IndexMut};
 
 #[derive(Clone, Debug)]
 pub struct Query<'s> {
@@ -11,7 +11,11 @@ pub struct Query<'s> {
 
 impl<'s> From<&'s str> for Query<'s> {
     fn from(kind: &'s str) -> Self {
-        Self { kind, text: kind, children: vec![] }
+        Self {
+            kind,
+            text: kind,
+            children: vec![],
+        }
     }
 }
 
@@ -27,7 +31,11 @@ impl<'s> From<(&tree_sitter::Node<'_>, &'s str)> for Query<'s> {
 
 impl<'s> Query<'s> {
     pub fn new(kind: &'s str, text: &'s str) -> Self {
-        Self { kind, text, children: vec![] }
+        Self {
+            kind,
+            text,
+            children: vec![],
+        }
     }
 
     pub fn add_child<Q: Into<Self>>(&mut self, child: Q) {
@@ -53,7 +61,7 @@ impl<'s> Query<'s> {
             }
             NodeKind::TokenPattern(pattern) => {
                 if !self.children.is_empty() {
-                    return None
+                    return None;
                 }
                 let captures = pattern.captures(self.text)?;
                 let mut matches = Matches::default();
@@ -66,7 +74,13 @@ impl<'s> Query<'s> {
                 matches.insert(name, self.text);
                 Some(matches)
             }
-            NodeKind::Not(node) => if self.matches(node).is_some() { None } else { Some(Matches::default()) },
+            NodeKind::Not(node) => {
+                if self.matches(node).is_some() {
+                    None
+                } else {
+                    Some(Matches::default())
+                }
+            }
         }
     }
 
@@ -115,8 +129,10 @@ impl<'a, 's> QuerySlice<'s, 'a> {
         let mut indices = query.indices();
         match modifier {
             NodeModifier::Child | NodeModifier::DirectChild => indices.push(0),
-            NodeModifier::Sibling | NodeModifier::DirectSibling => *indices.last_mut().unwrap() += 1,
-            NodeModifier::Also => {},
+            NodeModifier::Sibling | NodeModifier::DirectSibling => {
+                *indices.last_mut().unwrap() += 1
+            }
+            NodeModifier::Also => {}
         }
         Self { query, indices }
     }
@@ -133,9 +149,7 @@ impl<'a, 's> QuerySlice<'s, 'a> {
 
     fn pop(&self) -> Option<Self> {
         let mut indices = self.indices.clone();
-        if indices.pop().is_none() {
-            return None;
-        }
+        indices.pop()?;
         Some(Self {
             query: self.query,
             indices,
@@ -144,9 +158,12 @@ impl<'a, 's> QuerySlice<'s, 'a> {
 
     fn shift(&self) -> Option<Self> {
         let mut indices = self.indices.clone();
-        let last = indices.last_mut().unwrap();
+        let last = indices.last_mut()?;
         *last = last.checked_sub(1)?;
-        Some(Self { query: self.query, indices })
+        Some(Self {
+            query: self.query,
+            indices,
+        })
     }
 
     fn find_here<'k>(&self, node: &'k NodeKind) -> Result<(Self, Matches<'k, 's>), Option<Self>> {
@@ -159,36 +176,84 @@ impl<'a, 's> QuerySlice<'s, 'a> {
     }
 
     fn find_child<'k>(&self, node: &'k NodeKind) -> Option<(Self, Matches<'k, 's>)> {
-        match self.find_direct_child(node) {
-            Ok(result) => Some(result),
-            Err(query) => query?.find_child(node),
+        match node {
+            NodeKind::Not(node) => match self.find_child(node) {
+                None => Some((self.clone(), Matches::default())),
+                Some(..) => None,
+            },
+            node => match self.find_direct_child(node) {
+                Ok(result) => Some(result),
+                Err(query) => query?.find_child(node),
+            },
         }
     }
 
-    fn find_direct_child<'k>(&self, node: &'k NodeKind) -> Result<(Self, Matches<'k, 's>), Option<Self>> {
-        let index = self.pop().ok_or(None)?;
-        let query = &self.query[&index.indices];
-        if let Some(matches) = query.matches(node) {
-            Ok((index, matches))
-        } else {
-            Err(Some(index))
+    fn find_direct_child<'k>(
+        &self,
+        node: &'k NodeKind,
+    ) -> Result<(Self, Matches<'k, 's>), Option<Self>> {
+        match node {
+            NodeKind::Not(node) => {
+                let index = match self.pop() {
+                    Some(index) => index,
+                    None => return Ok((self.clone(), Matches::default())),
+                };
+                let query = &self.query[&index.indices];
+                match query.matches(node) {
+                    Some(..) => Err(Some(index)),
+                    None => Ok((index, Matches::default())),
+                }
+            }
+            node => {
+                let index = self.pop().ok_or(None)?;
+                let query = &self.query[&index.indices];
+                if let Some(matches) = query.matches(node) {
+                    Ok((index, matches))
+                } else {
+                    Err(Some(index))
+                }
+            }
         }
     }
 
     fn find_sibling<'k>(&self, node: &'k NodeKind) -> Option<(Self, Matches<'k, 's>)> {
-        match self.find_direct_sibling(node) {
-            Ok(result) => Some(result),
-            Err(query) => query?.find_sibling(node),
+        match node {
+            NodeKind::Not(node) => match self.find_sibling(node) {
+                None => Some((self.clone(), Matches::default())),
+                Some(..) => None,
+            },
+            node => match self.find_direct_sibling(node) {
+                Ok(result) => Some(result),
+                Err(query) => query?.find_sibling(node),
+            },
         }
     }
 
-    fn find_direct_sibling<'k>(&self, node: &'k NodeKind) -> Result<(Self, Matches<'k, 's>), Option<Self>> {
-        let index = self.shift().ok_or(None)?;
-        let query = &self.query[&index.indices];
-        if let Some(matches) = query.matches(node) {
-            Ok((index, matches))
-        } else {
-            Err(Some(index))
+    fn find_direct_sibling<'k>(
+        &self,
+        node: &'k NodeKind,
+    ) -> Result<(Self, Matches<'k, 's>), Option<Self>> {
+        match node {
+            NodeKind::Not(node) => {
+                let index = match self.shift() {
+                    Some(index) => index,
+                    None => return Ok((self.clone(), Matches::default())),
+                };
+                let query = &self.query[&index.indices];
+                match query.matches(node) {
+                    Some(..) => Err(Some(index)),
+                    None => Ok((index, Matches::default())),
+                }
+            }
+            node => {
+                let index = self.shift().ok_or(None)?;
+                let query = &self.query[&index.indices];
+                if let Some(matches) = query.matches(node) {
+                    Ok((index, matches))
+                } else {
+                    Err(Some(index))
+                }
+            }
         }
     }
 }
